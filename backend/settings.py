ee5eef967d6f4cf221945548c0a558a6acc03a80
quintus-web <ongangaquintus@gmail.com>
2026-03-11 21:16:12 +0300

    add

--- backend/settings.py
+++ backend/settings.py
@@ -86,8 +86,13 @@ DATABASES = {
     "default": dj_database_url.config(
         conn_max_age=600
     )
+ 
 }
-
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
